bcc97b197107264f3a764c5a60315aebca2b026f
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2024-05-09 11:35:28 +0300

    attempted solution

--- model.py
+++ model.py
@@ -1,17 +1,24 @@
-
-import torch
-import numpy as np
 import torch.nn as nn
-
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer1 = ...
+        #Add Linear NN layers
+        self.layer1 = nn.Linear(64*64,512)
+        self.layer2 = nn.Linear(512,512)
+        self.layer3 = nn.Linear(512,256)
+        self.layer4 = nn.Linear(256,10)
     
     def forward(self,x):
         
-        ...
+        #Pass x through the sequence of layers. Don't forget activation functions!
+        x = self.layer1(x)
+        x = nn.ReLU()(x)
+        x = self.layer2(x)
+        x = nn.ReLU()(x)
+        x = self.layer3(x)
+        x = nn.ReLU()(x)
+        x = self.layer4(x)
 
-        return out
+        return x
